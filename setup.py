c109b9abddff53bcb3cb0f86613bcec389c548cf
saghul <saghul@gmail.com>
2012-10-04 21:01:41 +0200

    Added setup.py and MANIFEST.in files

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from uvent import __version__
+
+setup(
+    name             = 'uvent',
+    version          = __version__,
+    url              = 'https://github.com/saghul/uvent',
+    author           = 'Saúl Ibarra Corretgé',
+    author_email     = 'saghul@gmail.com',
+    description      = 'A Gevent core implemented using libuv',
+    long_description = open('README.rst', 'r').read(),
+    packages         = ['uvent'],
+    classifiers      = [
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.0",
+          "Programming Language :: Python :: 3.1",
+          "Programming Language :: Python :: 3.2"
+    ]
+)
+
